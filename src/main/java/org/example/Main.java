6c5548ddea68e97ea272c549d5bca50ccb0af59b
Nikita <ratatyi04@gmail.com>
2024-07-21 23:26:17 +0500

    lab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,49 +1,17 @@
 package org.example;
 
-import java.io.*;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String dbPath = Paths.get("dataDB", "database.sqlite").toString();
-        String sqlsPath = Paths.get("sqls").toString();
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath)) {
-            if (conn != null) {
-                for (int i = 1; i <= 24; i++) {
-                    String sqlFileName = String.format("ex_%02d.sql", i);
-                    File sqlFile = Paths.get(sqlsPath, sqlFileName).toFile();
-
-                    if (sqlFile.exists()) {
-                        try (BufferedReader br = new BufferedReader(new FileReader(sqlFile))) {
-                            StringBuilder sb = new StringBuilder();
-                            String line;
-                            while ((line = br.readLine()) != null) {
-                                sb.append(line).append("\n");
-                            }
-                            String sql = sb.toString();
-
-                            try (Statement stmt = conn.createStatement()) {
-                                stmt.execute(sql);
-                                System.out.println("Executed " + sqlFileName);
-                            } catch (Exception e) {
-                                System.err.println("Error executing " + sqlFileName + ": " + e.getMessage());
-                            }
-                        } catch (Exception e) {
-                            System.err.println("Error reading " + sqlFileName + ": " + e.getMessage());
-                        }
-                    } else {
-                        System.err.println(sqlFileName + " does not exist.");
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("Error connecting to database: " + e.getMessage());
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
     }
 }
